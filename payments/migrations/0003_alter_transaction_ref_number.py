21b8c8e1f36c394457015ab0d13bf994c1f3d168
AHMZ <ahmz.ut@gmail.com>
2025-10-24 18:00:47 +0330

    Update Transaction model to change ref_number field type to BigIntegerField and add it to TransactionAdmin display

---
+++ payments/migrations/0003_alter_transaction_ref_number.py
@@ -0,0 +1,18 @@
+# Generated migration
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('payments', '0002_purchasingitem_color'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='transaction',
+            name='ref_number',
+            field=models.BigIntegerField(blank=True, null=True),
+        ),
+    ]
